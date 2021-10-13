ff0a25bbc7373bd1341e043428094e759ba3c578
Chris Lee <christopher.lee222@yahoo.com>
2021-10-13 09:21:18 -0500

    apexcharts added to dashboard hardcode data

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,42 @@
 import React from "react";
-import statusCards from '../assets/JsonData/status-card-data.json';
-import StatusCard from '../components/status-card/StatusCard';
+import { Link } from 'react-router-dom'
+import statusCards from "../assets/JsonData/status-card-data.json";
+import StatusCard from "../components/status-card/StatusCard";
+import Chart from 'react-apexcharts';
+
+const chartOptions = {
+  series: [
+    {
+      name: "Online Customers",
+      data: [40, 70, 20, 90, 35, 80, 30, 91, 22],
+    },
+    {
+      name: "Store Customers",
+      data: [40, 30, 50, 100, 22, 10, 30, 83, 54],
+    },
+  ],
+  options: {
+    color: ['#6ab04c', '#2980b9'], 
+    chart: {
+      background: 'transparent'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    legend: {
+      position: 'top'
+    },
+    grid: {
+      show: false
+    }
+  }
+};
 
 const Dashboard = () => {
   return (
@@ -9,22 +45,40 @@ const Dashboard = () => {
       <div className="row">
         <div className="col-6">
           <div className="row">
-            {
-              statusCards.map((item, index) => (
-                <div className="col-6" key={index}>
-                  <StatusCard 
-                    title = {item.title}
-                    icon = {item.icon}
-                    count = {item.count}
-                  />
-                </div>
-              ))
-            }
+            {statusCards.map((item, index) => (
+              <div className="col-6" key={index}>
+                <StatusCard
+                  title={item.title}
+                  icon={item.icon}
+                  count={item.count}
+                />
+              </div>
+            ))}
           </div>
         </div>
         <div className="col-6">
           <div className="card full-height">
             {/* chart */}
+            <Chart 
+              options={chartOptions.options}
+              series={chartOptions.series}
+              type='line'
+              height='100%'
+            />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card__header">
+              <h3>Top Customers</h3>
+            </div>
+            <div className="card__body">
+              {/* table */}
+              ff
+            </div>
+            <div className="card__footer">
+              <Link to='/customers'>View All</Link>
+            </div>
           </div>
         </div>
       </div>

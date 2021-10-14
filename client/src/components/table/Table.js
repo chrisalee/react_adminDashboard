498404b0c0f5da77cf44783028368f2f14f50335
Chris Lee <christopher.lee222@yahoo.com>
2021-10-13 23:33:41 -0500

    customers page and start of pagination for table

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Table.css';
 // import Customers from '../../assets/JsonData/customer-list.json';
 
 const Table = (props) => {
+
+    const initailDataShow = props.limit && props.bodyData ? props.bodyData.slice(0, Number(props.limit)) : props.bodyData;
+    const [dataShow, setDataShow] = useState(initailDataShow);
+
+    let pages = 1;
+    let range = [];
+    if(props.limit !== undefined) {
+        let page = Math.floor(props.bodyData.length / Number(props.limit));
+        pages = props.bodyData.length & Number(props.limit) === 0 ? page : page + 1;
+        range = [...Array(pages).keys()]
+    }
 
     return (
         <div className='table-wrapper'>
@@ -22,7 +33,7 @@ const Table = (props) => {
                     props.bodyData && props.renderBody ? (
                         <tbody>
                             {
-                                props.bodyData.map((item, index) => props.renderBody(item, index))
+                                dataShow.map((item, index) => props.renderBody(item, index))
                             }
                         </tbody>
                     ) : null
